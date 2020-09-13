034654ddff7005cce1667b4f3d9fba5d93fc52b3
Arctic Fox <fox@x1team.com>
2020-09-14 01:46:41 +0300

    1) small refactor

---
+++ src/main/java/com/ebay/demo/checker/model/Consts.java
@@ -0,0 +1,8 @@
+package com.ebay.demo.checker.model;
+
+public class Consts {
+    public static final int END_HOUR = 23;
+    public static final int START_HOUR = 8;
+    public static final int TWO_HOURS = 2;
+    public static final int DAYS_IN_WEEK = 7;
+}
--- src/main/java/com/ebay/demo/checker/service/impl/SchedulerService.java
+++ src/main/java/com/ebay/demo/checker/service/impl/SchedulerService.java
@@ -1,11 +1,11 @@
 package com.ebay.demo.checker.service.impl;
 
+import com.ebay.demo.checker.model.Consts;
 import com.ebay.demo.checker.model.auction.AuctionRequest;
 import com.ebay.demo.checker.model.auction.AuctionRequestReponce;
 import com.ebay.demo.checker.model.scheduler.SchedulerTask;
 import com.ebay.demo.checker.model.scheduler.SchedulerTaskStatus;
 import com.ebay.demo.checker.service.ISchedulerService;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +19,8 @@ import java.util.concurrent.*;
 @Slf4j
 @Service
 public class SchedulerService implements ISchedulerService {
+
+
 
     private ConcurrentLinkedQueue<SchedulerTask> tasks = new ConcurrentLinkedQueue<>();
 
@@ -43,6 +45,7 @@ public class SchedulerService implements ISchedulerService {
         executorService.scheduleWithFixedDelay(resend, 2_000, 30_000, TimeUnit.MICROSECONDS);
     }
 
+    //this code resend failed tasks from the queue
     private void resendAuctions(){
         //Can use parallelStream
         int counter=0;
@@ -57,14 +60,15 @@ public class SchedulerService implements ISchedulerService {
     }
 
     // gives date that is not already full (looking to the next day)
+    //and not in the night
     private LocalDateTime getSuitableDateTime(LocalDateTime dateTime){
 
         LocalDateTime localDateTime = overlappingTime.get(dateTime);
         if(localDateTime!=null){
-            dateTime = dateTime.plusHours(2).plusMinutes(1);
+            dateTime = dateTime.plusHours(Consts.TWO_HOURS).plusMinutes(1);
         }
 
-        if(dateTime.getHour()>=23 || dateTime.getHour()<8){
+        if(dateTime.getHour()>= Consts.END_HOUR || dateTime.getHour()< Consts.START_HOUR){
             dateTime = dateTime.plusDays(1);
             dateTime = getStartingHourOfScheduler(dateTime);
         }
@@ -94,44 +98,47 @@ public class SchedulerService implements ISchedulerService {
 
             AuctionRequestReponce auctionRequestReponce = new AuctionRequestReponce();
             //if task is fired for first time
+
+            AuctionRequest auctionRequest = null;
+
+            LocalDateTime fromTime=null;
+            LocalDateTime  toTime=null;
             if(schedulerTask==null){
 
                 //starting from 8pm
-                LocalDateTime fromTime = getStartingHourOfScheduler(LocalDateTime.now());
+                fromTime = getStartingHourOfScheduler(LocalDateTime.now());
                 fromTime = getSuitableDateTime(fromTime);
-                LocalDateTime toTime = fromTime.plus(2, ChronoUnit.HOURS);
+                toTime = fromTime.plus(Consts.TWO_HOURS, ChronoUnit.HOURS);
 
-                AuctionRequest auctionRequest = new AuctionRequest();
-                auctionRequest.setFromTime(fromTime);
-                auctionRequest.setToTime(toTime);
+                auctionRequest = new AuctionRequest();
                 auctionRequest.setItemId(itemId);
-                auctionRequestReponce.setAuctionRequest(auctionRequest);
 
             } else {
 
-                AuctionRequest auctionRequest =  schedulerTask.getAuctionRequest();
+                auctionRequest =  schedulerTask.getAuctionRequest();
 
-                LocalDateTime fromTime = schedulerTask.getAuctionRequest().getFromTime();
+                fromTime = schedulerTask.getAuctionRequest().getFromTime();
                 fromTime = getSuitableDateTime(fromTime);
-
-                fromTime = fromTime.plus(2, ChronoUnit.HOURS)
+                fromTime = fromTime.plus(Consts.TWO_HOURS, ChronoUnit.HOURS)
                         .plus(1, ChronoUnit.MINUTES);
 
-                LocalDateTime toTime = fromTime.plus(2, ChronoUnit.HOURS);
-
-                auctionRequest.setFromTime(fromTime);
-                auctionRequest.setToTime(toTime);
-
-                auctionRequestReponce.setAuctionRequest(auctionRequest);
+                toTime = fromTime.plus(Consts.TWO_HOURS, ChronoUnit.HOURS);
             }
+
+            auctionRequest.setFromTime(fromTime);
+            auctionRequest.setToTime(toTime);
+            auctionRequestReponce.setAuctionRequest(auctionRequest);
 
             auctionRequestReponce = auctionCreatorService.createSingleAuction(auctionRequestReponce);
             String error = auctionRequestReponce.getAuctionResponse().getError();
 
+            //via error types we are mapping dates and time to be full (not usable for us).
             if(!StringUtils.isEmpty(error)){
                 if(error.contains("DayFullAuctionException")){
+
                     LocalDate fromDate=  auctionRequestReponce.getAuctionRequest().getFromTime().toLocalDate();
                     fullDates.put(fromDate, fromDate);
+
                 }
                 else if(error.contains("WeekFullAuctionException")){
 
@@ -139,9 +146,12 @@ public class SchedulerService implements ISchedulerService {
                     addFullWeek(fromDate);
 
                 }else if(error.contains("OverlappingException")){
-                    LocalDateTime fromTime=  auctionRequestReponce.getAuctionRequest().getFromTime();
-                    overlappingTime.put(fromTime, fromTime);
+
+                    LocalDateTime overlappingFromTime=  auctionRequestReponce.getAuctionRequest().getFromTime();
+                    overlappingTime.put(overlappingFromTime, overlappingFromTime);
+
                 }
+
                 SchedulerTask st = schedulerTask;
                 if(st==null){
                     st=new SchedulerTask();
@@ -151,10 +161,6 @@ public class SchedulerService implements ISchedulerService {
                 st.setAuctionRequest(auctionRequestReponce.getAuctionRequest());
 
                 tasks.add(st);
-            }else {
-//                if(schedulerTask!=null) {
-//                    tasks.remove(schedulerTask);
-//                }
             }
         };
 
@@ -162,7 +168,7 @@ public class SchedulerService implements ISchedulerService {
     }
 
     private void addFullWeek(LocalDate fromDate) {
-        int day = 7 - fromDate.getDayOfWeek().getValue();
+        int day = Consts.DAYS_IN_WEEK - fromDate.getDayOfWeek().getValue();
         for (int i = 0; i <= day; i++) {
             LocalDate ld = fromDate.plusDays(i);
             fullDates.put(ld, ld);
